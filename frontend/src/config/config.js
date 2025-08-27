947e901ecede480079d65f91d8b8b1c4dad3f88b
hifdzuliman <hifdzul.iman@soluix.ai>
2025-08-28 00:05:56 +0700

    change env

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
+  API_BASE_URL: process.env.REACT_APP_API_URL || 'http://178.128.54.249:8080/api',
   APP_NAME: 'Web Work Request',
   VERSION: '1.0.0'
 };
